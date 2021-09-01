fa72cc4ce40d36c00c9aebe317a779684897d37f
stkirk <shaanekirk@gmail.com>
2021-09-01 16:25:43 -0400

    wrote tests for Show component to render and display fake show data, loading state, and number of season options in slect input

--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -1,27 +1,45 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-import Show from './../Show';
+import Show from "./../Show";
 
 const testShow = {
-    //add in approprate test data structure here.
-}
+  //add in approprate test data structure here.
+  name: "Fake Show",
+  summary: "Fake show summary",
+  seasons: [
+    { id: 0, name: "Fake Season 1", episodes: [] },
+    { id: 1, name: "Fake Season 2", episodes: [] },
+    { id: 2, name: "Fake Season 3", episodes: [] },
+  ],
+};
 
-test('renders testShow and no selected Season without errors', ()=>{
+test("renders testShow and no selected Season without errors", () => {
+  render(<Show show={testShow} selectedSeason={"none"} />);
 });
 
-test('renders Loading component when prop show is null', () => {
+test("renders Loading component when prop show is null", () => {
+  render(<Show show={null} />);
+
+  const loading = screen.getByTestId("loading-container");
+
+  expect(loading).toHaveTextContent(/fetching data.../i);
+  expect(loading).toBeTruthy();
+  expect(loading).toBeVisible();
 });
 
-test('renders same number of options seasons are passed in', ()=>{
+test("renders same number of options seasons are passed in", () => {
+  render(<Show show={testShow} selectedSeason={"none"} />);
+
+  const selectOptions = screen.getAllByTestId("season-option");
+
+  expect(selectOptions).toHaveLength(3);
 });
 
-test('handleSelect is called when an season is selected', () => {
-});
+test("handleSelect is called when an season is selected", () => {});
 
-test('component renders when no seasons are selected and when rerenders with a season passed in', () => {
-});
+test("component renders when no seasons are selected and when rerenders with a season passed in", () => {});
 
 //Tasks:
 //1. Build an example data structure that contains the show data in the correct format. A show should contain a name, a summary and an array of seasons, each with a id, name and (empty) list of episodes within them. Use console.logs within the client code if you need to to verify the structure of show data.
