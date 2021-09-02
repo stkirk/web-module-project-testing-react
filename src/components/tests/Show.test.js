843f4b6529b133c3b19633db74a7b15a9eab9111
stkirk <shaanekirk@gmail.com>
2021-09-02 12:06:56 -0400

    wrote final Display test

--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { getByRole, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 import Show from "./../Show";
